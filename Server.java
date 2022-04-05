065a30aebec78442ad76a9e50e9784f59af36536
walt <williamswaltaf@gmail.com>
2022-04-04 20:28:23 -0500

    Revert "Merge pull request #1 from WaltAFWilliams/jacque-t-patch-1"
    This reverts commit 3f113977cc3072f064af9825eadedf816779a818, reversing
    changes made to df60ee81915ecfc7e6e63046f2c0a37f3444609c.

--- Server.java
+++ Server.java
@@ -1,34 +1,55 @@
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.List;
-import java.util.Vector;
 
 public class Server{
-    protected static List<Socket> sockets = new Vector<>();
 
-    public static void main(String[] args) throws IOException{
-        ServerSocket serverSocket = new ServerSocket(1111);
-        boolean done = false;
+	private ServerSocket serverSocket;
 
-        while(!done){
-            try{
-                System.out.println("Binding to port, please wait  ...");
-                System.out.println("Waiting for a client ...");
-                Socket accept = serverSocket.accept();
-                synchronized(sockets){
-                    sockets.add(accept);
-                }
+	public Server(ServerSocket serverSocket){
+		this.serverSocket = serverSocket;
+	}
 
-                Thread thread = new Thread(new ServerThead(accept));
-                thread.start();
-            }
-            catch (Exception e){
-                done = true;
-                e.printStackTrace();
-            }
-        }
-        serverSocket.close();
-    }
+	public void startServer(){
 
+		try {
+
+			while (!serverSocket.isClosed()){
+
+				Socket clientSocket = serverSocket.accept();
+				System.out.println("A new client has connected");
+				ClientHandler clientHandler = new ClientHandler(clientSocket);
+
+				Thread thread = new Thread(clientHandler);
+				thread.start();
+
+			}
+
+		} catch (IOException e){
+
+
+		}
+
+
+	}
+
+	public void closeServerSocket(){
+
+		try {
+			if (serverSocket != null) {
+				serverSocket.close();
+
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+
+		ServerSocket serverSocket = new ServerSocket(1111);
+		Server server = new Server(serverSocket);
+		server.startServer();
+
+	}
 }
